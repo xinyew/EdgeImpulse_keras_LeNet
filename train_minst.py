7ee6862a0905bb4cbd6fca4cd5eb9ac68dd739e2
xinyew <wangxinye1996@outlook.com>
2023-04-10 20:35:55 -0400

    ready for modifying

---
+++ train_minst.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets, layers, models, losses
+
+(x_train,y_train),(x_test,y_test) = datasets.mnist.load_data()
+print(x_train.shape)
+
+# x_train = tf.pad(x_train, [[0, 0], [2,2], [2,2]])/255
+# x_test = tf.pad(x_test, [[0, 0], [2,2], [2,2]])/255
+# print(x_train.shape)
+
+
+x_train = tf.expand_dims(x_train, axis=3, name=None)
+x_test = tf.expand_dims(x_test, axis=3, name=None)
+print(x_train.shape)
+
+x_val = x_train[-2000:,:,:,:]
+y_val = y_train[-2000:]
+x_train = x_train[:-2000,:,:,:]
+y_train = y_train[:-2000]
+
+model = models.Sequential()
+model.add(layers.Conv2D(6, 5, activation='tanh', input_shape=x_train.shape[1:]))
+model.add(layers.AveragePooling2D(2))
+model.add(layers.Activation('sigmoid'))
+model.add(layers.Conv2D(16, 5, activation='tanh'))
+model.add(layers.AveragePooling2D(2))
+model.add(layers.Activation('sigmoid'))
+model.add(layers.Conv2D(120, 4, activation='tanh'))
+model.add(layers.Flatten())
+model.add(layers.Dense(84, activation='tanh'))
+model.add(layers.Dense(10, activation='softmax'))
+model.summary()
+
+
+model.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+history = model.fit(x_train, y_train, batch_size=64, epochs=10, validation_data=(x_val, y_val))
+
+fig, axs = plt.subplots(2, 1, figsize=(15,15))
+
+axs[0].plot(history.history['loss'])
+axs[0].plot(history.history['val_loss'])
+axs[0].title.set_text('Training Loss vs Validation Loss')
+axs[0].legend(['Train', 'Val'])
+
+axs[1].plot(history.history['accuracy'])
+axs[1].plot(history.history['val_accuracy'])
+axs[1].title.set_text('Training Accuracy vs Validation Accuracy')
+axs[1].legend(['Train', 'Val'])
+
+model.evaluate(x_test, y_test)
